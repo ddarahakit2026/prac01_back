0274b180307ae276998d73561922b1894869883d
lee <sunyeoplee0@gmail.com>
2026-02-25 09:44:50 +0900

    feat/ 응답 코딩 컨벤션 적용

---
+++ src/main/java/com/example/demo/common/model/BaseResponseStatus.java
@@ -0,0 +1,20 @@
+package com.example.demo.common.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public enum BaseResponseStatus {
+
+    SUCCESS(true, 2000, "여청이 성공했습니다"),
+
+    JWT_EXPIRED(false, 3001, "JWT 토큰이 만료되었습니다."),
+    JWT_INVALID(false, 3002, "JWT 토큰이 유효하지 않습니다."),
+    SIGNUP_DUPLICATE_EMAIL(false, 3003, "중복된 이메일입니다."),
+    SIGNUP_INVALUD(false, 3004, "중복된 이메일입니다.");
+
+    private final boolean success;
+    private final int code;
+    private final String message;
+}
