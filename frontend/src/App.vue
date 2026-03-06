5049ac2dd97254084aa9859e1b5599bcede99b38
ddarahakit2026 <ddarahakit2026@gmail.com>
2026-03-06 14:41:47 +0900

    [Feat] 프론트에 로그인 기능 및 서버 프록시 설정 추가

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -4,10 +4,14 @@ import axios from 'axios'
 
 const message = ref('');
 const socket = ref(null);
+const user = ref({
+  email: '',
+  password: ''
+})
 
 const subscribePush = async () => {
   const permission = await Notification.requestPermission()
-  if(permission !== 'granted') {
+  if (permission !== 'granted') {
     console.log("권한 없음")
 
     return
@@ -18,7 +22,7 @@ const subscribePush = async () => {
 
   const registration = await navigator.serviceWorker.ready;
   let subscription = await registration.pushManager.getSubscription()
-  if(!subscription) {
+  if (!subscription) {
     subscription = await registration.pushManager.subscribe({
       userVisibleOnly: true,
       applicationServerKey: VAPID_PUBLIC_KEY
@@ -31,7 +35,7 @@ const subscribePush = async () => {
 
 }
 const connectWebSocket = () => {
-  const ws = new WebSocket("ws://localhost:8080/ws")
+  const ws = new WebSocket("ws://localhost:5173/ws")
   socket.value = ws;
 
   ws.onmessage = (message) => {
@@ -41,13 +45,24 @@ const connectWebSocket = () => {
 const sendMessage = () => {
   socket.value.send(JSON.stringify(message.value))
 }
+
+const login = async () => {
+  await axios.post("http://localhost:5173/api/user/login", user.value);
+}
 </script>
 
 <template>
   <button @click="connectWebSocket">웹 소켓 연결</button>
-  <input name="message" v-model="message" />
+  <input name="message" v-model="message"/>
   <button @click="sendMessage">메시지 전송</button>
+  <hr>
+
   <button @click="subscribePush">알림 구독</button>
+
+  <hr>
+  <input name="email" v-model="user.email"/>
+  <input name="password" v-model="user.password"/>
+  <button @click="login">로그인</button>
 </template>
 
 <style scoped></style>
